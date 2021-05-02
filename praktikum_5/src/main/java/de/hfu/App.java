85f1529aa98a0f9fb5cd21ff97edbcbe3001d42a
Sabine Schleise <sabine.schleise@hs-furtwangen.de>
2021-05-02 15:22:01 +0200

    did goal: site in INtellij

--- praktikum_5/src/main/java/de/hfu/App.java
+++ praktikum_5/src/main/java/de/hfu/App.java
@@ -5,9 +5,14 @@ public class App
 {
     public static void main( String[] args )
     {
-     Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    /**
+     * Ein Scanner wird erstellt um die Worte einzulesen und sie in Großbuchstaben zurückzugeben
+     */
+    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
     System.out.println("Enter a word and you will  get it in uppercase");
-
+    /**
+    * Der Scanner liest die Zeile ein und gibt diese in Uppercase wieder aus
+    */
     String word = myObj.nextLine();  // Read user input
     System.out.println(word.toUpperCase());  // Output user input
     }
