7eb118c333a54ec306b7241928b9c23cfe97de38
Sabine Schleise <sabine.schleise@hs-furtwangen.de>
2021-05-20 18:49:10 +0200

    worked on Stub ResidentRespository

---
+++ praktikum_5/src/test/java/de/hfu/residents/IntegrationsTest.java
@@ -0,0 +1,22 @@
+package de.hfu.residents;
+
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.List;
+
+import de.hfu.residents.repository.ResidentRespositoryStub;
+import org.junit.Test;
+
+import de.hfu.residents.domain.Resident;
+import de.hfu.residents.repository.ResidentRespositoryStub;
+import de.hfu.residents.service.BaseResidentService;
+import de.hfu.residents.service.ResidentServiceException;
+public class IntegrationsTest {
+    ResidentRespositoryStub respositoryStub = new ResidentRespositoryStub();
+
+
+}
